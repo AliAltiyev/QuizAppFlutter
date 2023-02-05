89038041253dd81e1bda6b1c9f0db52e20e2c7f3
AliAltyyev <alidroid696@gmial.com>
2023-02-05 22:56:34 +0300

    Formatting code

--- lib/pages/quiz_page.dart
+++ lib/pages/quiz_page.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app_flutter/widgets/question_widget.dart';
-import 'package:quiz_app_flutter/widgets/true_button_widget.dart';
-
-import '../widgets/false_button_widget.dart';
 
 class QuizPage extends StatefulWidget {
   const QuizPage({super.key});
@@ -12,16 +9,70 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  late List<Widget> answers;
+
+  @override
+  void initState() {
+    super.initState();
+    answers = [];
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: const [QuestionWidget(), TrueButton(), FalseButton()],
+            children: [
+              const QuestionWidget(),
+              _answerButton('True', 1),
+              _answerButton('False', 2),
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Row(
+                    children: answers,
+                  ),
+                ),
+              )
+            ],
           ),
         ));
+  }
+
+  Widget _answerButton(String text, int choice) {
+    return Expanded(
+      flex: 1,
+      child: Padding(
+        padding: const EdgeInsets.all(24),
+        child: ElevatedButton(
+            style: ButtonStyle(
+                backgroundColor: (choice == 1)
+                    ? MaterialStatePropertyAll(Colors.green)
+                    : MaterialStatePropertyAll(Colors.red)),
+            onPressed: () {
+              setState(() {
+                (choice == 1)
+                    ? answers.add(const Icon(
+                        Icons.check,
+                        size: 24,
+                        color: Colors.green,
+                      ))
+                    : answers.add(const Icon(
+                        Icons.face,
+                        color: Colors.red,
+                        size: 24,
+                      ));
+              });
+            },
+            child: Text(
+              text,
+              style: const TextStyle(fontSize: 16, color: Colors.white),
+            )),
+      ),
+    );
   }
 }
--- lib/widgets/true_button_widget.dart
+++ lib/widgets/true_button_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
 class TrueButton extends StatefulWidget {
-  const TrueButton({Key? key}) : super(key: key);
+  List<Widget> answers = [];
+
+  TrueButton({Key? key, required this.answers}) : super(key: key);
 
   @override
   State<TrueButton> createState() => _TrueButtonState();
@@ -10,19 +12,6 @@ class TrueButton extends StatefulWidget {
 class _TrueButtonState extends State<TrueButton> {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 1,
-      child: Padding(
-        padding: const EdgeInsets.all(24),
-        child: ElevatedButton(
-            style: const ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(Colors.green)),
-            onPressed: () {},
-            child: const Text(
-              'True',
-              style: TextStyle(fontSize: 16, color: Colors.white),
-            )),
-      ),
-    );
+    return Placeholder();
   }
 }
