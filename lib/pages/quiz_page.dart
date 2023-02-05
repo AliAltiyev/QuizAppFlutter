3b66652cb373011f96120c19af7cd6ebfbc23501
AliAltyyev <alidroid696@gmial.com>
2023-02-05 20:48:39 +0300

    Initial commit

---
+++ lib/pages/quiz_page.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+class QuizPage extends StatefulWidget {
+  const QuizPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<QuizPage> createState() => _QuizPageState();
+}
+
+class _QuizPageState extends State<QuizPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(),
+        body: Column(
+          children: [],
+        ));
+  }
+}
